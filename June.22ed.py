f2e17ea2cfd0225305f8e3769932b3c00c03d35d
3194libin <15764747737@qq.com>
2019-06-22 11:46:21 +0800

    多重继承

--- June.22ed.py
+++ June.22ed.py
@@ -1,30 +1,18 @@
 import random as r
 
 class Fish:
-    def __init__(self):
-        self.x = r.randint(0,10)
-        self.y = r.randint(0,10)
-    def move(self):
-         self.x -= 1
-         print('我的位置是：',self.x,self.y)
-
-class GoldFish(Fish):
+    def fish(self):
+        print("我是一条小鱼")
+class GoldFish:
+    def goldfish(self):
+        print("我是一条金鱼")
+class Carp:
+    def carpfish(self):
+        print("我是一条鳗鱼")
+class Shark(Fish,GoldFish,Carp):
     pass
-class Carp(Fish):
-    pass
-class Shark(Fish):
-    def __init__(self):
-        super().__init__()
-        self.hungry = True
-
-    def eat(self):
-        if self.hungry:
-            print('我是个吃货')
-            self.hungry = False
-        else:
-            print('我吃饱了')
 
 s = Shark()
-s.move()
-s.eat()
-s.move()
+s.carpfish()
+s.fish()
+s.goldfish()
